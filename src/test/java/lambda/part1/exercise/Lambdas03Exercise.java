5f5ad1a5d5b4857b17081c0db2636fe398d83d44
Dmitrii Kuznetsov <dmitrii_kuznetsov@epam.com>
2016-12-14 00:25:24 +0300

    Homework 01 - Dmitrii Kuznetsov

--- src/test/java/lambda/part1/exercise/Lambdas03Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas03Exercise.java
@@ -18,21 +18,29 @@ public class Lambdas03Exercise {
 
     @Test
     public void generic0() {
-        final GenericProduct<Integer> prod = null; // Use anonymous class
+        final GenericProduct<Integer> prod = new GenericProduct<Integer>() {
+            @Override
+            public Integer prod(Integer a, int i) {
+                return a * i;
+            }
+        }; // Use anonymous class
 
         assertEquals(prod.prod(3, 2), Integer.valueOf(6));
     }
 
     @Test
     public void generic1() {
-        final GenericProduct<Integer> prod = null; // Use statement lambda
+        final GenericProduct<Integer> prod =
+                (Integer a, int i) -> {
+                    return a * i;
+                }; // Use statement lambda
 
         assertEquals(prod.prod(3, 2), Integer.valueOf(6));
     }
 
     @Test
     public void generic2() {
-        final GenericProduct<Integer> prod = null; // Use expression lambda
+        final GenericProduct<Integer> prod = (a, i) -> a * i; // Use expression lambda
 
         assertEquals(prod.prod(3, 2), Integer.valueOf(6));
     }
@@ -47,9 +55,13 @@ public class Lambdas03Exercise {
 
     @Test
     public void strSum() {
-        final GenericProduct<String> prod = null; // use stringProd;
+        final GenericProduct<String> prod = Lambdas03Exercise::stringProd; // use stringProd;
+        final GenericProduct<String> prod1 = (a, i) -> stringProd(a, i);
+        final GenericProduct<String> prod2 = (a, i) -> {return stringProd(a, i);};
 
         assertEquals(prod.prod("a", 2), "aa");
+        assertEquals(prod1.prod("a", 2), "aa");
+        assertEquals(prod2.prod("a", 2), "aa");
     }
 
     private final String delimeter = "-";
@@ -64,7 +76,7 @@ public class Lambdas03Exercise {
 
     @Test
     public void strSum2() {
-        final GenericProduct<String> prod = null; // use stringSumWithDelimeter;
+        final GenericProduct<String> prod = this::stringSumWithDelimeter; // use stringSumWithDelimeter;
 
         assertEquals(prod.prod("a", 3), "a-a-a");
     }
