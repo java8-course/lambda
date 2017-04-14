b5f04b22c489d53c9d305eb627a93251df5cbee0
Ilia Onishchenko <ilia_onishchenko@epam.com>
2017-04-14 12:17:25 +0300

    Lambdas01Exercise implemented.

--- src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
@@ -1,15 +1,17 @@
 package lambda.part1.exercise;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 import com.google.common.collect.ImmutableList;
 import data.Person;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import java.util.stream.Stream;
 
 public class Lambdas01Exercise {
 
@@ -21,7 +23,24 @@ public class Lambdas01Exercise {
                 new Person("name 2", "lastName 1", 30)
         };
 
-        // TODO use Arrays.sort
+        //        Java 7
+//        Arrays.sort(persons, new Comparator<Person>() {
+//            @Override
+//            public int compare(Person o1, Person o2) {
+//                return o1.getAge() - o2.getAge();
+//            }
+//        });
+
+//        Java 8
+        persons = Stream.of(persons)
+            .sorted(Comparator.comparing(Person::getAge))
+            .toArray(Person[]::new);
+
+        assertThat(persons, is(new Person[]{
+            new Person("name 3", "lastName 3", 20),
+            new Person("name 2", "lastName 1", 30),
+            new Person("name 1", "lastName 2", 40),
+            }));
 
         assertArrayEquals(persons, new Person[]{
                 new Person("name 3", "lastName 3", 20),
@@ -40,7 +59,24 @@ public class Lambdas01Exercise {
 
         Person person = null;
 
-        // TODO use FluentIterable
+        //        Java 7
+//        final Optional<Person> personOptional =
+//                FluentIterable.from(persons)
+//                .firstMatch(new Predicate<Person>() {
+//                    @Override
+//                    public boolean apply(Person p) {
+//                        return p.getAge() == 30;
+//                    }
+//                });
+//
+//        if(personOptional.isPresent())
+//            person = personOptional.get();
+
+//        Java 8
+        person = persons.stream()
+            .filter(p -> p.getAge() ==30)
+            .findFirst()
+            .orElse(new Person("n","ln", 99999));
 
         assertEquals(person, new Person("name 1", "lastName 2", 30));
     }
