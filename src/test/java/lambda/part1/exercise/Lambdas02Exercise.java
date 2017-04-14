60e3854dcfc4aed2b4ae79018c27612548550482
Ilia Onishchenko <ilia_onishchenko@epam.com>
2017-04-14 12:18:39 +0300

    Lambdas02Exercise implemented.

--- src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
@@ -1,13 +1,16 @@
 package lambda.part1.exercise;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import data.Person;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class Lambdas02Exercise {
     @Test
@@ -18,7 +21,7 @@ public class Lambdas02Exercise {
                 new Person("name 2", "lastName 1", 30)
         };
 
-        // TODO use Arrays.sort
+        Arrays.sort(persons, (p1, p2) -> p1.getAge() - p2.getAge());
 
         assertArrayEquals(persons, new Person[]{
                 new Person("name 3", "lastName 3", 20),
@@ -37,7 +40,12 @@ public class Lambdas02Exercise {
 
         Person person = null;
 
-        // TODO use FluentIterable
+        final Optional<Person> personOptional =
+            FluentIterable.from(persons)
+                .firstMatch(p -> p.getAge() == 30);
+
+        if(personOptional.isPresent())
+            person = personOptional.get();
 
         assertEquals(person, new Person("name 1", "lastName 2", 30));
     }
