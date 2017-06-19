051497ca695e3c787508ac1c20b623ec368e30fe
sidmalex@yandex.ru <11216623>
2017-06-19 22:42:14 +0300

    * sortPersonsByAge using Arrays.sort and lambda

--- src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 import data.Person;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -19,6 +20,10 @@ public class Lambdas02Exercise {
         };
 
         // TODO use Arrays.sort
+        Arrays.sort(persons, ((o1, o2) ->
+                o1.getAge() < o2.getAge()
+                        ? -1
+                        : o1.getAge() > o2.getAge() ? 1 : 0));
 
         assertArrayEquals(persons, new Person[]{
                 new Person("name 3", "lastName 3", 20),
