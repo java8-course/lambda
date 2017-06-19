186ad729196d983ec5ecf908e755b4ee39d1d174
bvvvd <chrisburrrma@gmail.com>
2017-06-20 00:39:58 +0300

    Lambdas03Exercise  generic2 +

--- src/test/java/lambda/part1/exercise/Lambdas03Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas03Exercise.java
@@ -39,7 +39,7 @@ public class Lambdas03Exercise {
 
     @Test
     public void generic2() {
-        final GenericProduct<Integer> prod = null; // Use expression lambda
+        final GenericProduct<Integer> prod = (Integer a, int i) -> a * i; // Use expression lambda
 
         assertEquals(prod.prod(3, 2), Integer.valueOf(6));
     }
