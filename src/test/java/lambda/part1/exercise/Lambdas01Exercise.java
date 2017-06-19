72f36ff2a6a276477b0f4a3325b91bed839ca3c2
Afli <il.gusevskiy@gmail.com>
2017-06-19 23:47:30 +0300

    Feat: findFirstWithAge30() in Lambdas01Exercise released using com.google.common.collect.FluentIterable

--- src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
@@ -1,13 +1,13 @@
 package lambda.part1.exercise;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import data.Person;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -43,6 +43,11 @@ public class Lambdas01Exercise {
         Person person = null;
 
         // TODO use FluentIterable
+        Optional<Person> personOptional = FluentIterable.from(persons)
+                .firstMatch(p -> p.getAge() == 30);
+        if (personOptional.isPresent()){
+            person = personOptional.get();
+        }
 
         assertEquals(person, new Person("name 1", "lastName 2", 30));
     }
