4725f4c85e8343683f0979b6d265723537b814c5
sidmalex@yandex.ru <11216623>
2017-06-19 22:55:25 +0300

    * strSum2

--- src/test/java/lambda/part1/exercise/Lambdas03Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas03Exercise.java
@@ -73,7 +73,7 @@ public class Lambdas03Exercise {
 
     @Test
     public void strSum2() {
-        final GenericProduct<String> prod = null; // use stringSumWithDelimeter;
+        final GenericProduct<String> prod = this::stringSumWithDelimeter; // use stringSumWithDelimeter;
 
         assertEquals(prod.prod("a", 3), "a-a-a");
     }
