3b8ff1fe70e33a7a42c8db9bbe796f4c3df11212
Yegor <zakovryash@gmail.com>
2017-06-21 20:10:17 +0300

    compare fixed

--- src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
+++ src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
@@ -7,6 +7,7 @@ import data.Person;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -22,7 +23,7 @@ public class Lambdas02Exercise {
         };
 
         // TODO use Arrays.sort
-        Arrays.sort(persons, (o1, o2) -> o1.getAge() - o2.getAge());
+        Arrays.sort(persons, Comparator.comparingInt(Person::getAge));
 
         assertArrayEquals(persons, new Person[]{
                 new Person("name 3", "lastName 3", 20),
