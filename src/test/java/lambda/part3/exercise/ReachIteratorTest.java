31b942cec545e918db53adcffc5df0a583dc88be
Leo.Scream <mod.satyr@gmail.com>
2017-06-22 23:21:20 +0300

    + ReachIterator initial commit

---
+++ src/test/java/lambda/part3/exercise/ReachIteratorTest.java
@@ -0,0 +1,20 @@
+package lambda.part3.exercise;
+
+/**
+ * @author Denis Verkhoturov, mod.satyr@gmail.com
+ */
+public class ReachIteratorTest {
+    interface ReachIterable<T> {
+
+        // forNext(T -> void) -> boolean
+
+        // filter
+        // map
+        // flatMap
+
+        // anyMatch (T -> boolean) -> boolean
+        // allMatch (T -> boolean) -> boolean
+        // noneMatch (T -> boolean) -> boolean
+        // firstMatch(T -> boolean) -> Optional<T>
+    }
+}
