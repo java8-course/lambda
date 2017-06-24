c9003fbe41b1fd6750ffb708e55179fda934ff0c
Kirill Volkov <neborodulin@gmail.com>
2017-06-24 10:09:21 +0300

    Traversable fixed2

--- src/test/java/lambda/part3/exercise/TraversableMapping.java
+++ src/test/java/lambda/part3/exercise/TraversableMapping.java
@@ -1,6 +1,5 @@
 package lambda.part3.exercise;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
@@ -15,8 +14,8 @@ public class TraversableMapping {
 
         void forEach(Consumer<T> c);
 
-        default <R> lambda.part3.exercise.TraversableMapping.Traversable<R> map(Function<T, R> f) {
-            lambda.part3.exercise.TraversableMapping.Traversable<T> self = this;
+        default <R> Traversable<R> map(Function<T, R> f) {
+            Traversable<T> self = this;
 
             return new Traversable<R>() {
                 @Override
@@ -26,13 +25,13 @@ public class TraversableMapping {
             };
         }
 
-        default <R> lambda.part3.exercise.TraversableMapping.Traversable<R> flatMap(Function<T, Traversable<R>> mapper){
+        default <R> Traversable<R> flatMap(Function<T, Traversable<R>> mapper){
 
             return consumer -> this.forEach(t -> mapper.apply(t).forEach(consumer));
 
         }
 
-        default lambda.part3.exercise.TraversableMapping.Traversable<T> filter(Predicate<T> predicate){
+        default Traversable<T> filter(Predicate<T> predicate){
             return consumer -> this.forEach(item -> {
                 if (predicate.test(item)) consumer.accept(item);
             });
@@ -44,7 +43,7 @@ public class TraversableMapping {
         }
 
 
-        static <T> lambda.part3.exercise.TraversableMapping.Traversable<T> from (List<T> list){
+        static <T> Traversable<T> from (List<T> list){
             return list::forEach;
         }
     }
