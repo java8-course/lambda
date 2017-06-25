7e0954a0497370b5af2c0173d1061cd4b6e1e904
Anton Barybin <anton_barybin@epam.com>
2017-06-26 00:26:24 +0300

    lazy mapping

--- src/test/java/lambda/part3/exercise/Mapping.java
+++ src/test/java/lambda/part3/exercise/Mapping.java
@@ -133,7 +133,12 @@ public class Mapping {
 
     private static class LazyMapHelper<T, R> {
 
+        private List<T> list;
+        private Function<T, R> function;
+
         public LazyMapHelper(List<T> list, Function<T, R> function) {
+            this.list = list;
+            this.function = function;
         }
 
         public static <T> LazyMapHelper<T, T> from(List<T> list) {
@@ -142,12 +147,16 @@ public class Mapping {
 
         public List<R> force() {
             // TODO
-            throw new UnsupportedOperationException();
+            List<R> result = new ArrayList<R>();
+            list.forEach(e -> result.add(function.apply(e)));
+            return result;
+            //throw new UnsupportedOperationException();
         }
 
         public <R2> LazyMapHelper<T, R2> map(Function<R, R2> f) {
             // TODO
-            throw new UnsupportedOperationException();
+            return new LazyMapHelper<T, R2>(list, function.andThen(f));
+//            throw new UnsupportedOperationException();
         }
 
     }
@@ -215,12 +224,15 @@ public class Mapping {
 
         final List<Employee> mappedEmployees =
                 LazyMapHelper.from(employees)
+                        .map(e -> e.withPerson(e.getPerson().withFirstName("John")))
+                        .map(e -> e.withJobHistory(addOneYear(e.getJobHistory())))
+                        .map(e -> e.withJobHistory(replace(e.getJobHistory())))
                 /*
                 .map(TODO) // change name to John
                 .map(TODO) // add 1 year to experience duration
                 .map(TODO) // replace qa with QA
                 * */
-                .force();
+                        .force();
 
         final List<Employee> expectedResult =
                 Arrays.asList(
