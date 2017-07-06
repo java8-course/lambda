5b3c6e0b5a994407eeeae3169210805f204515f1
Andrei Andronov <Andrei_Andronov@epam.com>
2017-07-06 17:30:11 +0300

    part 3 Traversable flatMap and filter were done. ReachIterable is in process

--- src/test/java/lambda/part3/exercise/Mapping.java
+++ src/test/java/lambda/part3/exercise/Mapping.java
@@ -9,7 +9,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.assertEquals;
 
@@ -143,45 +145,130 @@ public class Mapping {
             Function<T, R2> newFunction = function.andThen(f);
             return new LazyMapHelper<>(list, newFunction);
         }
-
     }
 
-    private static class LazyFlatMapHelper<T, R> {
+    interface  ReachIterable<T> {
+        //hasNext and next
+        boolean tryGet(Consumer<T> c);
 
-        public LazyFlatMapHelper(List<T> list, Function<T, List<R>> function) {
-        }
 
-        public static <T> LazyFlatMapHelper<T, T> from(List<T> list) {
-            throw new UnsupportedOperationException();
-        }
+        //TODO boolean anyMatch(Predicate<T> p)
+        //TODO boolean allMatch(Predicate<T> p)
+        //TODO boolean noneMatch(Predicate<T> p)
 
-        public List<R> force() {
-            // TODO
-            throw new UnsupportedOperationException();
-        }
+        //TODO Optional<T> firstMatch(Predicate<T> p)
 
-        // TODO filter
-        // (T -> boolean) -> (T -> [T])
-        // filter: [T1, T2] -> (T -> boolean) -> [T2]
-        // flatMap": [T1, T2] -> (T -> [T]) -> [T2]
-
-        public <R2> LazyFlatMapHelper<T, R2> map(Function<R, R2> f) {
-            final Function<R, List<R2>> listFunction = rR2TorListR2(f);
-            return flatMap(listFunction);
-        }
-
-        // (R -> R2) -> (R -> [R2])
-        private <R2> Function<R, List<R2>> rR2TorListR2(Function<R, R2> f) {
-            throw new UnsupportedOperationException();
-        }
-
-        // TODO *
-        public <R2> LazyFlatMapHelper<T, R2> flatMap(Function<R, List<R2>> f) {
-            throw new UnsupportedOperationException();
+        static <T> ReachIterable<T> from(List<T> list) {
+            return new ReachIterable<T>() {
+                @Override
+                public boolean tryGet(Consumer<T> c) {
+                    return false;
+                }
+            };
         }
     }
 
+    interface Traversable<T> {
+        void forEach(Consumer<T> c);
 
+        default Traversable<T> filter(Predicate<T> p) {
+            final Traversable<T> self = this;
+            return new Traversable<T>() {
+                @Override
+                public void forEach(Consumer<T> c) {
+                    self.forEach(t -> {
+                        if (p.test(t))
+                            c.accept(t);
+                    });
+                }
+            };
+        }
+
+        default <R> Traversable<R> flatMap(Function<T, List<R>> f) {
+            final Traversable<T> self = this;
+            return new Traversable<R>() {
+                @Override
+                public void forEach(Consumer<R> c) {
+                    self.forEach(
+                            t -> f.apply(t)
+                                    .forEach(c));
+                }
+            };
+        }
+
+        default <R> Traversable<R> map(Function<T, R> f) {
+            final Traversable<T> self = this;
+            return new Traversable<R>() {
+                @Override
+                public void forEach(Consumer<R> c) {
+                    self.forEach(t -> c.accept(f.apply(t)));
+                }
+            };
+        }
+
+        default List<T> toList() {
+            final List<T> list = new ArrayList<>();
+            forEach(list::add);
+            return list;
+        }
+
+        static <T> Traversable<T> from(List<T> list) {
+            return new Traversable<T>() {
+                @Override
+                public void forEach(Consumer<T> c) {
+                    list.forEach(c);
+                }
+            };
+        }
+    }
+
+    @Test
+    public void testFlatMap() {
+        final List<Employee> employees =
+                Arrays.asList(
+                        new Employee(
+                                new Person("a", "Galt", 30),
+                                Arrays.asList(
+                                        new JobHistoryEntry(2, "dev", "epam"),
+                                        new JobHistoryEntry(1, "dev", "google")
+                                )),
+                        new Employee(
+                                new Person("b", "Doe", 40),
+                                Arrays.asList(
+                                        new JobHistoryEntry(3, "qa", "yandex"),
+                                        new JobHistoryEntry(1, "qa", "epam"),
+                                        new JobHistoryEntry(1, "dev", "abc")
+                                ))
+                );
+
+        System.err.println(
+                Arrays.toString(
+                        Traversable.from(employees).flatMap(Employee::getJobHistory).toList().toArray()));
+    }
+
+    @Test
+    public void testFilter() {
+        final List<Employee> employees =
+                Arrays.asList(
+                        new Employee(
+                                new Person("a", "Galt", 30),
+                                Arrays.asList(
+                                        new JobHistoryEntry(2, "dev", "epam"),
+                                        new JobHistoryEntry(1, "dev", "google")
+                                )),
+                        new Employee(
+                                new Person("b", "Doe", 40),
+                                Arrays.asList(
+                                        new JobHistoryEntry(3, "qa", "yandex"),
+                                        new JobHistoryEntry(1, "qa", "epam"),
+                                        new JobHistoryEntry(1, "dev", "abc")
+                                ))
+                );
+
+        System.err.println(
+                Arrays.toString(
+                        Traversable.from(employees).filter(e -> e.getPerson().getFirstName().equals("a")).toList().toArray()));
+    }
 
     @Test
     public void lazy_mapping() {
