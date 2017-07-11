93df2f21bad1703ad2766491163dc6356f4fe6b9
Ilia Lisitsyn <txt.ilya@gmail.com>
2017-07-11 13:46:37 +0300

    LazyFlatMapHelper

--- src/test/java/lambda/part3/exercise/Mapping.java
+++ src/test/java/lambda/part3/exercise/Mapping.java
@@ -81,7 +81,7 @@ public class Mapping {
                 .map(TODO) // add 1 year to experience duration .map(e -> e.withJobHistory(addOneYear(e.getJobHistory())))
                 .map(TODO) // replace qa with QA
                 * */
-                .getList();
+                        .getList();
 
         final List<Employee> expectedResult =
                 Arrays.asList(
@@ -131,17 +131,20 @@ public class Mapping {
     }
 
     private static class LazyFlatMapHelper<T, R> {
+        private final List<T> list;
+        private final Function<T, List<R>> function;
 
         public LazyFlatMapHelper(List<T> list, Function<T, List<R>> function) {
+            this.list = list;
+            this.function = function;
         }
 
         public static <T> LazyFlatMapHelper<T, T> from(List<T> list) {
-            throw new UnsupportedOperationException();
+            return new LazyFlatMapHelper<>(list, Collections::singletonList);
         }
 
         public List<R> force() {
-            // TODO
-            throw new UnsupportedOperationException();
+            return new MapHelper<T>(list).flatMap(function).getList();
         }
 
         // TODO filter
@@ -150,21 +153,20 @@ public class Mapping {
         // flatMap": [T1, T2] -> (T -> [T]) -> [T2]
 
         public <R2> LazyFlatMapHelper<T, R2> map(Function<R, R2> f) {
-            final Function<R, List<R2>> listFunction = rR2TorListR2(f);
+            final Function<R, List<R2>> listFunction = f.andThen(Collections::singletonList);
             return flatMap(listFunction);
         }
 
-        // (R -> R2) -> (R -> [R2])
-        private <R2> Function<R, List<R2>> rR2TorListR2(Function<R, R2> f) {
-            throw new UnsupportedOperationException();
+        public LazyFlatMapHelper<T, R> filter(Predicate<R> p) {
+            return flatMap(r -> p.test(r) ? Collections.singletonList(r) : Collections.emptyList());
         }
 
         // TODO *
         public <R2> LazyFlatMapHelper<T, R2> flatMap(Function<R, List<R2>> f) {
-            throw new UnsupportedOperationException();
+            Function<T, List<R2>> listFunction = t -> new MapHelper<>(function.apply(t)).flatMap(f).getList();
+            return new LazyFlatMapHelper<>(list, listFunction);
         }
     }
-
 
 
     @Test
@@ -198,7 +200,7 @@ public class Mapping {
                 .map(TODO) // add 1 year to experience duration
                 .map(TODO) // replace qa with QA
                 * */
-                .force();
+                        .force();
 
         final List<Employee> expectedResult =
                 Arrays.asList(
