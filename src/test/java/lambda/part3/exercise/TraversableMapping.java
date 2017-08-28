6422695ebfccb22888f74385c3ccfb71e460c4e2
Kirill Volkov <neborodulin@gmail.com>
2017-08-28 09:51:57 +0300

    Update TraversableMapping.java

--- src/test/java/lambda/part3/exercise/TraversableMapping.java
+++ src/test/java/lambda/part3/exercise/TraversableMapping.java
@@ -14,26 +14,22 @@ public class TraversableMapping {
 
         void forEach(Consumer<T> c);
 
-        default <R> Traversable<R> map(Function<T, R> f) {
-            Traversable<T> self = this;
-
-            return new Traversable<R>() {
-                @Override
-                public void forEach(Consumer<R> c) {
-                    self.forEach(t -> c.accept(f.apply(t)));
-                }
+        default <R> Traversable<R> map(Function<T, R> mapper) {
+            return consumer -> this.forEach(item -> consumer.accept(mapper.apply(item)));
             };
         }
 
         default <R> Traversable<R> flatMap(Function<T, Traversable<R>> mapper){
 
-            return consumer -> this.forEach(t -> mapper.apply(t).forEach(consumer));
+            return consumer -> this.forEach(element -> mapper.apply(element).forEach(consumer));
+            return consumer -> this.forEach(consumer);
 
         }
 
         default Traversable<T> filter(Predicate<T> predicate){
             return consumer -> this.forEach(item -> {
-                if (predicate.test(item)) consumer.accept(item);
+                if (predicate.test(item)) con
+                    sumer.accept(item);
             });
         }
         default List<T> force(){
